09fb849be2182f8bf9fc58558827f7c97d6ef4cc
alex <debryniuk@gmail.com>
2021-03-22 14:34:47 +0200

    auth Reducer in ts

--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -4,23 +4,26 @@ import {ThunkDispatch} from 'redux-thunk';
 
 const SET_USER_DATA = "SET_USER_DATA";
 
-type MyState = {
-    userId: null | number,
-    email: null | string,
-    login: null | string,
-    isAuth: boolean,
+// type MyStateType = {
+//     userId: null | number,
+//     email: null | string,
+//     login: null | string,
+//     isAuth: boolean,
+// }
+
+const initialState = {
+    userId: null as null | number,
+    email: null as null | string,
+    login: null as null | string,
+    isAuth: false as boolean,
 }
 
-const initialState: MyState = {
-    userId: null,
-    email: null,
-    login: null,
-    isAuth: false,
-}
+type MyStateType = typeof initialState;
 
-interface MyAction{ type: string, payload: object }
+// type PayloadType = { userId: number, email: string, login: string, isAuth: boolean };
+type MyAction = { type: typeof SET_USER_DATA, payload?: MyStateType };
 
-const authReducer = (state: MyState = initialState, action: MyAction) => {
+const authReducer = (state: MyStateType = initialState, action: MyAction): MyStateType => {
     switch (action.type) {
         case SET_USER_DATA:
             return {
@@ -33,18 +36,28 @@ const authReducer = (state: MyState = initialState, action: MyAction) => {
 }
 
 export const setAuthUserData = (userId: number | null, email: string | null,
-                                login: string | null, isAuth: boolean) => (
-    {
+                                login: string | null, isAuth: boolean):
+    MyAction => {
+    return {
         type: SET_USER_DATA,
         payload: {userId, email, login, isAuth}
     }
-);
+}
 
-// type response
+type AuthResponse = {
+    data: {
+        resultCode: number,
+        data: {
+            userId: number;
+            email: string;
+            login: string;
+        }
+    }
+}
 
 export const authMe = () => {
-    return (dispatch: ThunkDispatch<MyState, void, MyAction>) => {
-        return authAPI.authMe().then(res => {
+    return (dispatch: ThunkDispatch<MyStateType, void, MyAction>) => {
+        return authAPI.authMe().then((res: AuthResponse) => {
             if (res.data.resultCode === 0) {
                 const {login, userId, email} = res.data.data;
                 dispatch(setAuthUserData(userId, email, login, true));
@@ -53,9 +66,22 @@ export const authMe = () => {
     }
 }
 
+type LoginResponse = {
+    data: {
+        resultCode: number,
+        data: {
+            userId: number;
+            email: string;
+            login: string;
+        }
+    }
+}
+
+type LoginRes = { data: { resultCode: number, messages: Array<string> } };
+
 export const login = (email: string, password: string, rememberMe = false) => {
-    return (dispatch: ThunkDispatch<MyState, void, MyAction>) => {
-        authAPI.login(email, password, rememberMe).then(res => {
+    return (dispatch: ThunkDispatch<MyStateType, void, MyAction>) => {
+        authAPI.login(email, password, rememberMe).then((res: LoginRes) => {
             if (res.data.resultCode === 0) {
                 dispatch(authMe())
             } else {
@@ -67,8 +93,8 @@ export const login = (email: string, password: string, rememberMe = false) => {
 }
 
 export const logout = () => {
-    return (dispatch: ThunkDispatch<MyState, void, MyAction>) => {
-        authAPI.logout().then(res => {
+    return (dispatch: ThunkDispatch<MyStateType, void, MyAction>) => {
+        authAPI.logout().then((res: LoginRes) => {
             if (res.data.resultCode === 0) {
                 dispatch(setAuthUserData(null, null, null, false));
             }
